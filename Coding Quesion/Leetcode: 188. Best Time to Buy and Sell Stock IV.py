efbb4bf5b9261513483e6a82f3bcb0782a368069
Gangxin-Li <lgx.uofg@gmail.com>
2023-02-28 10:42:09 +0000

    188

--- Coding Quesion/Leetcode: 188. Best Time to Buy and Sell Stock IV.py
+++ Coding Quesion/Leetcode: 188. Best Time to Buy and Sell Stock IV.py
@@ -15,7 +15,7 @@
 # Input: k = 2, prices = [3,2,6,5,0,3]
 # Output: 7
 # Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4. Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
- 
+
 
 # Constraints:
 
@@ -26,10 +26,13 @@ from typing import List
 
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
+        # Create a init array to maintain the data
         dp = [[-float('inf'),0] for _ in range(k+1)]
         for price in prices:
             for i in range(1,k+1):
+                # sell it and we get the profile
                 dp[i][1] = max(dp[i][1],dp[i][0] + price )
+                # When we hold it, and the profile we get.
                 dp[i][0] = max(dp[i][0], dp[i-1][1] - price )
         # print(dp[k])
         return dp[k][1]
